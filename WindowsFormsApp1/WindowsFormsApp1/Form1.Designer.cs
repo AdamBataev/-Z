2919d7515e863e923ab98d34fce018b59e5190b6
AdamBataev <144354736+AdamBataev@users.noreply.github.com>
2023-09-28 22:57:03 +0400

    init app

---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.Designer.cs
@@ -0,0 +1,92 @@
+﻿namespace WindowsFormsApp1
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.buttonCalculate = new System.Windows.Forms.Button();
+            this.textBoxArray = new System.Windows.Forms.TextBox();
+            this.labelSumResult = new System.Windows.Forms.TextBox();
+            this.labelProductResult = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // buttonCalculate
+            // 
+            this.buttonCalculate.Location = new System.Drawing.Point(629, 354);
+            this.buttonCalculate.Name = "buttonCalculate";
+            this.buttonCalculate.Size = new System.Drawing.Size(135, 69);
+            this.buttonCalculate.TabIndex = 0;
+            this.buttonCalculate.Text = "Калькуляция";
+            this.buttonCalculate.UseVisualStyleBackColor = true;
+            this.buttonCalculate.Click += new System.EventHandler(this.buttonCalculate_Click);
+            // 
+            // textBoxArray
+            // 
+            this.textBoxArray.Location = new System.Drawing.Point(38, 27);
+            this.textBoxArray.Name = "textBoxArray";
+            this.textBoxArray.Size = new System.Drawing.Size(100, 22);
+            this.textBoxArray.TabIndex = 1;
+            // 
+            // labelSumResult
+            // 
+            this.labelSumResult.Location = new System.Drawing.Point(38, 160);
+            this.labelSumResult.Name = "labelSumResult";
+            this.labelSumResult.Size = new System.Drawing.Size(100, 22);
+            this.labelSumResult.TabIndex = 2;
+            // 
+            // labelProductResult
+            // 
+            this.labelProductResult.Location = new System.Drawing.Point(144, 160);
+            this.labelProductResult.Name = "labelProductResult";
+            this.labelProductResult.Size = new System.Drawing.Size(100, 22);
+            this.labelProductResult.TabIndex = 3;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.labelProductResult);
+            this.Controls.Add(this.labelSumResult);
+            this.Controls.Add(this.textBoxArray);
+            this.Controls.Add(this.buttonCalculate);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button buttonCalculate;
+        private System.Windows.Forms.TextBox textBoxArray;
+        private System.Windows.Forms.TextBox labelSumResult;
+        private System.Windows.Forms.TextBox labelProductResult;
+    }
+}
+
---
+++ WindowsFormsApp1/WindowsFormsApp1/Form1.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp1
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void buttonCalculate_Click(object sender, EventArgs e)
+        {
+            // считываем введенный пользователем массив чисел
+            string[] inputArray = textBoxArray.Text.Split(' ');
+
+            // инициализируем переменные
+            int sum = 0;
+            int product = 1;
+
+            // ищем минимальный и максимальный элементы
+            int minIndex = 0;
+            int maxIndex = 0;
+            for (int i = 0; i < inputArray.Length; i++)
+            {
+                int currentValue = int.Parse(inputArray[i]);
+                if (currentValue < int.Parse(inputArray[minIndex]))
+                {
+                    minIndex = i;
+                }
+                if (currentValue > int.Parse(inputArray[maxIndex]))
+                {
+                    maxIndex = i;
+                }
+
+                // суммируем положительные элементы
+                if (currentValue > 0)
+                {
+                    sum += currentValue;
+                }
+            }
+
+            // вычисляем произведение чисел между минимальным и максимальным элементами
+            int startIndex = Math.Min(minIndex, maxIndex) + 1;
+            int endIndex = Math.Max(minIndex, maxIndex);
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                product *= int.Parse(inputArray[i]);
+            }
+
+            // выводим результаты
+            labelSumResult.Text = sum.ToString();
+            labelProductResult.Text = product.ToString();
+        }
+    }
+}
